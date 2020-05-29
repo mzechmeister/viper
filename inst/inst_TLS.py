288c702bd10808be5aa2cafb0a3256823f84b1f2
Mathias Zechmeister <zechmeister@astro.physik.uni-goettingen.de>
2020-05-29 12:51:27 +0200

    feat: TLS spectra can be read

--- inst/inst_TLS.py
+++ inst/inst_TLS.py
@@ -1,14 +1,15 @@
 from astropy.io import fits
-import readmultispec
+from readmultispec import readmultispec
 # see ttps://github.com/mzechmeister/serval/blob/master/src/inst_FIES.py
 
 filename = '../TLS/data/BETA_GEM.fits'
 hdu = fits.open(filename)[0]
-f_i = hdu.data[33]
+f = hdu.data[33]
 #w_i = 6128.8833940969 + 0.05453566108124*np.arange(f_i.size)  # guess
 #w_i, f_i = np.loadtxt('data/BETA_GEM.order34.dat').T
 
-w = readmultispec(filename, reform=True, quiet=True)
+gg = readmultispec(filename, reform=True, quiet=True)
       # "".join(self.header['WAT2_0*'].values()).split("spec")
-      #w = airtovac(gg['wavelen'][orders])
+w = gg['wavelen'] #[orders] # air or vac?
+
 print(w[33])
