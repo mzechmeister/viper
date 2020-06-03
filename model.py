a44c71933c3267c7d38f939b9376df54e1a6021d
Mathias Zechmeister <zechmeister@astro.physik.uni-goettingen.de>
2020-06-03 21:47:36 +0200

    refactor: Model isolated and converted into a class

---
+++ model.py
@@ -0,0 +1,51 @@
+import numpy as np
+from scipy import interpolate
+
+from gplot import *
+gplot.tmp = '$'
+
+c = 3e5   # [km/s] speed of light
+
+# IP sampling in velocity space
+# index k for IP space
+def IP(vk, s=2.2):
+    IP_k = np.exp(-(vk/s)**2)  # Gauss IP
+    IP_k /= IP_k.sum()           # normalise IP
+    return IP_k
+
+
+class model:
+    '''
+    The forward model
+    
+    '''
+     
+    def __init__(self, *args, IP_hw=50):
+        self.S_star, self.xj, self.iod_j, self.IP = args
+        # convolving with IP will reduce the valid wavelength range
+        self.dx = self.xj[1] - self.xj[0]  # sampling in uniform resampled Iod
+        self.vk = np.arange(-IP_hw,IP_hw+1) * self.dx * c
+        self.xj_eff = self.xj[IP_hw:-IP_hw]
+        print("sampling [km/s]:", self.dx*c)
+
+    def __call__(self, i, v, a, b, s):
+        # wavelength solution
+        xi = np.log(np.poly1d(b[::-1])(i))
+        # IP convolution
+        Sj_eff = np.convolve(self.IP(self.vk, s), self.S_star(self.xj+v/c) * self.iod_j, mode='valid')
+        # sampling to pixel
+        Si_eff = interpolate.interp1d(self.xj_eff, Sj_eff)(xi)
+        # flux normalisation
+        Si_mod = np.poly1d(a[::-1])(xi) * Si_eff
+        return Si_mod
+
+
+def show_model(x, y, ymod, res=True):
+    gplot(x, y, ymod, 'w lp pt 7 ps 0.5 t "S_i",',
+          '"" us 1:3w lp pt 6 ps 0.5 lc 3 t "S(i)"')
+    if res:
+        rms = np.std(y-ymod)
+        gplot.mxtics().mytics().my2tics()
+        # overplot residuals
+        gplot.y2range('[-0.2:2]').ytics('nomirr').y2tics()
+        gplot+(x, y-ymod, "w p pt 7 ps 0.5 lc 1 axis x1y2 t 'res %.3g', 0 lc 3 axis x1y2" % rms)
--- viper.py
+++ viper.py
@@ -13,6 +13,9 @@ from astropy.io import fits
 from gplot import *
 gplot.tmp = '$'
 
+from inst.inst_TLS import Spectrum
+from model import model, IP, show_model
+
 c = 3e5   # [km/s] speed of light
 
 
@@ -50,15 +53,10 @@ s = slice(*np.searchsorted(w_I2, [lmin, lmax]))
 gplot(w_I2[s], f_I2[s], 'w l lc 9')
 
 #### data TLS
-from inst.inst_TLS import Spectrum
 w, f = Spectrum(obsname)
-#hdu = fits.open(root_FTS+'BETA_GEM.fits')[0]
-#hdu = fits.open('lib/TLS/observations/SPECTRA/TV00007.fits')[0]
-#f_i = hdu.data[33]
 w_i = w[o]
 f_i = f[o]
 i = np.arange(f_i.size)
-#w_i = 6128.8833940969 + 0.05453566108124*np.arange(f_i.size)  # guess
 
 
 #####  stellar template   ####
@@ -94,50 +92,19 @@ gplot(w_I2[s], f_I2[s]/1.18, 'w l lc 9,', w_tpl[s_s]*(1+12/c), f_tpl[s_s], 'w l 
 xj = np.linspace(np.log(lmin)+100/c, np.log(lmax)-100/c, w_I2[s].size)  # reduce range by 100 km/s
 iod_j = interpolate.interp1d(np.log(w_I2), f_I2)(xj)
 
-dx = xj[1] - xj[0]  # sampling in uniform resampled Iod
-print("sampling [km/s]:", dx*c)
 
 # convert PESPI data into a function
 S_star = interpolate.interp1d(np.log(w_tpl), f_tpl)
 
-
-# IP sampling in velocity space
-# index k for IP space
-def IP(s=2.2):
-    vk = np.arange(-50,50+1) * dx * c
-    IP_k = np.exp(-(vk/s)**2)  # Gauss IP
-    IP_k /= IP_k.sum()           # normalise IP
-    return IP_k
-
-IP_k = IP()
+S_mod = model(S_star, xj, iod_j, IP)
 
 # plot
-gplot(vk, IP_k)
+gplot(S_mod.vk, S_mod.IP(S_mod.vk))
 
 # plot again, now the stellar template can be interpolated
 gplot(np.exp(xj), iod_j, S_star(xj), 'w l lc 9, "" us 1:3 w l lc 3')
 
-# convolving with IP will reduce the valid wavelength range
-cut = int(IP_k.size/2)
-xj_eff = xj[cut:-cut]
-
-# forward model
-
-def _S_eff(v):
-    #S_eff(ln(lam)) = IP(v) x ((a0+a1*ln(lam))*S_PEPSI(ln(lam)+v_star/c) * G_I2(ln(lam)))
-    #S_eff(ln(lam)) = IP(v) x ((A+ln(lan) )*S_PEPSI(ln(lam)+v_star/c) * G_I2(ln(lam))) 
-    # discrete supersampled effective spectrum
-    Sj_eff = np.convolve(IP_k, S_star(xj+v/c) * iod_j, mode='valid')
-    
-    # return continous supersampled effective spectrum
-    return interpolate.interp1d(xj_eff, Sj_eff)
-
-
-# a forward model for RV shift 3 km/s
-S_eff = _S_eff(v=3)
-
-
-gplot(np.exp(xj), iod_j, S_star(xj+3/c), 'w l lc 9 t "iodine", "" us 1:3 w l lc 3 t "template + 3 km/s (PEPSI)",', np.exp(xj_eff), S_eff(xj_eff), 'w l lc 1 t "IP x (tpl*I2)"')
+#gplot(np.exp(xj), iod_j, S_star(xj+3/c), 'w l lc 9 t "iodine", "" us 1:3 w l lc 3 t "template + 3 km/s",', np.exp(xj_eff), S_eff(xj_eff), 'w l lc 1 t "IP x (tpl*I2)"')
 
 # Now wavelength solution
 
@@ -151,39 +118,17 @@ s_obs = slice(*np.searchsorted(np.log(w_i), [xj[0]+100/c, xj[-1]-100/c]))
 
 # well, we see the wavelength solution can be improved
 
-gplot(i[s_obs], S_eff(np.log(lam(i[s_obs]))), 'w l,', i, f_i, 'w lp pt 7 ps 0.5 lc 3')
+#gplot(i[s_obs], S_eff(np.log(lam(i[s_obs]))), 'w l,', i, f_i, 'w lp pt 7 ps 0.5 lc 3')
 
 # and let's plot the observation against wavelength
 
-gplot(np.exp(xj_eff), S_eff(xj_eff), 'w l,', lam(i), f_i, 'w lp pt 7 ps 0.5 lc 3')
+#gplot(np.exp(xj_eff), S_eff(xj_eff), 'w l,', lam(i), f_i, 'w lp pt 7 ps 0.5 lc 3')
 
 v=0
 a = [0.96]
 b = [w_i[0], (w_i[-1]-w_i[0])/w_i.size] # [6128.8833940969, 0.05453566108124]
 s = 2.5
 
-def S_mod(i, v, a, b, s):
-    '''A forward model'''
-    # wavelength solution
-    xi = np.log(np.poly1d(b[::-1])(i))
-    # IP convolution
-    Sj_eff = np.convolve(IP(s), S_star(xj+v/c) * iod_j, mode='valid')
-    # sampling to pixel
-    Si_eff = interpolate.interp1d(xj_eff, Sj_eff)(xi)
-    # flux normalisation
-    Si_mod = np.poly1d(a[::-1])(xi) * Si_eff
-    return Si_mod
-
-
-def show_model(x, y, ymod, res=True):
-    gplot(x, y, ymod, 'w lp pt 7 ps 0.5 t "S_i",',
-          '"" us 1:3w lp pt 6 ps 0.5 lc 3 t "S(i)"')
-    if res:
-        rms = np.std(y-ymod)
-        gplot.mxtics().mytics().my2tics()
-        # overplot residuals
-        gplot.y2range('[-0.2:2]').ytics('nomirr').y2tics()
-        gplot+(x, y-ymod, "w p pt 7 ps 0.5 lc 1 axis x1y2 t 'res %.3g', 0 lc 3 axis x1y2" % rms)
 
 # a simple call to the forward model
 Si_mod = S_mod(i[s_obs], v=0, a=[1], b=b, s=s)
@@ -201,9 +146,8 @@ show_model(i[s_obs], f_i[s_obs], S_a(i[s_obs],*a), res=False)
 # A wrapper to fit the wavelength solution
 S_b = lambda x, b0,b1,b2,b3: S_mod(x, v, a, [b0,b1,b2,b3], s)
 
-n = 1/1.00027
-v = -2.+(n-1)*c   # a good guess for the stellar RV is needed
-bg = np.polyfit(i[s_obs], w_i[s_obs]/n, 3)[::-1]
+v = -2.   # a good guess for the stellar RV is needed
+bg = np.polyfit(i[s_obs], w_i[s_obs], 3)[::-1]
 b, e_b = curve_fit(S_b, i[s_obs], f_i[s_obs], p0=bg)
 bg1 = b*1
 
