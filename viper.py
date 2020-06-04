ae18997cefb080fe1e7a1c6d819c8f2462a22830
Mathias Zechmeister <zechmeister@astro.physik.uni-goettingen.de>
2020-06-04 09:03:15 +0200

    chore: Start guess optimised, output of e_rv (40m/s, rms=2.3%)

--- viper.py
+++ viper.py
@@ -42,10 +42,6 @@ if __name__ == "__main__":
 ####  FTS  ####
 w_I2, f_I2 = FTS()
 
-# display
-s = slice(*np.searchsorted(w_I2, [lmin, lmax]))
-gplot(w_I2[s], f_I2[s], 'w l lc 9')
-
 ####  data TLS  ####
 w_i, f_i = Spectrum(obsname, o=o)
 i = np.arange(f_i.size)
@@ -57,14 +53,14 @@ w_tpl, f_tpl = Tpl(tplname, o=o)
 lmin = max(w_tpl[0], w_i[0], w_I2[0])
 lmax = min(w_tpl[-1], w_i[-1], w_I2[-1])
 
+
+# display
+# pre-look raw input
 s = slice(*np.searchsorted(w_I2, [lmin, lmax]))
 s_s = slice(*np.searchsorted(w_tpl, [lmin, lmax]))
-gplot(w_I2[s], f_I2[s], 'w l lc 9,', w_tpl[s_s], f_tpl[s_s], 'w l lc 3')
 
-
-# pre-look data
 gplot(w_I2[s], f_I2[s], 'w l lc 9,', w_tpl[s_s], f_tpl[s_s], 'w l lc 3,', w_i, f_i, 'w lp lc 1 pt 7 ps 0.5')
-gplot(w_I2[s], f_I2[s]/1.18, 'w l lc 9,', w_tpl[s_s]*(1+12/c), f_tpl[s_s], 'w l lc 3,', w_i, f_i/1.04, 'w lp lc 1 pt 7 ps 0.5')
+#gplot(w_I2[s], f_I2[s]/1.18, 'w l lc 9,', w_tpl[s_s]*(1+12/c), f_tpl[s_s], 'w l lc 3,', w_i, f_i/1.04, 'w lp lc 1 pt 7 ps 0.5')
 
 
 # prepare input; convert discrete data to model
@@ -74,16 +70,19 @@ xj = np.linspace(np.log(lmin)+100/c, np.log(lmax)-100/c, w_I2[s].size)  # reduce
 iod_j = interpolate.interp1d(np.log(w_I2), f_I2)(xj)
 
 
-# convert PESPI data into a function
+# convert discrete template into a function
 S_star = interpolate.interp1d(np.log(w_tpl), f_tpl)
 
+
+# setup the model
 S_mod = model(S_star, xj, iod_j, IP)
 
-# plot
+# plot the IP
 gplot(S_mod.vk, S_mod.IP(S_mod.vk))
 
-# plot again, now the stellar template can be interpolated
-gplot(np.exp(xj), iod_j, S_star(xj), 'w l lc 9, "" us 1:3 w l lc 3')
+if 0:
+   # plot again, now the stellar template can be interpolated
+   gplot(np.exp(xj), iod_j, S_star(xj), 'w l lc 9, "" us 1:3 w l lc 3')
 
 #gplot(np.exp(xj), iod_j, S_star(xj+3/c), 'w l lc 9 t "iodine", "" us 1:3 w l lc 3 t "template + 3 km/s",', np.exp(xj_eff), S_eff(xj_eff), 'w l lc 1 t "IP x (tpl*I2)"')
 
@@ -94,22 +93,23 @@ gplot(np.exp(xj), iod_j, S_star(xj), 'w l lc 9, "" us 1:3 w l lc 3')
 #lam(x) = b0 + b_1 * x + b_2 * x**2
 
 lam = np.poly1d([w_i[0], (w_i[-1]-w_i[0])/w_i.size][::-1])
+
+# trim the observation to a range valid for the model
 s_obs = slice(*np.searchsorted(np.log(w_i), [xj[0]+100/c, xj[-1]-100/c]))
 
 
 # well, we see the wavelength solution can be improved
-
 #gplot(i[s_obs], S_eff(np.log(lam(i[s_obs]))), 'w l,', i, f_i, 'w lp pt 7 ps 0.5 lc 3')
 
 # and let's plot the observation against wavelength
 
 #gplot(np.exp(xj_eff), S_eff(xj_eff), 'w l,', lam(i), f_i, 'w lp pt 7 ps 0.5 lc 3')
 
-v=0
+# a parameter set
+v = 0
 a = [0.96]
 b = [w_i[0], (w_i[-1]-w_i[0])/w_i.size] # [6128.8833940969, 0.05453566108124]
 s = 2.5
-
 
 # a simple call to the forward model
 Si_mod = S_mod(i[s_obs], v=0, a=[1], b=b, s=s)
@@ -119,9 +119,7 @@ show_model(i[s_obs], f_i[s_obs], Si_mod, res=False)
 
 # A wrapper to fit the continuum
 S_a = lambda x, a0: S_mod(x, v, [a0], b, s)
-
 a, e_a = curve_fit(S_a, i[s_obs], f_i[s_obs])
-
 show_model(i[s_obs], f_i[s_obs], S_a(i[s_obs],*a), res=False)
 
 # A wrapper to fit the wavelength solution
@@ -139,16 +137,26 @@ gplot+(i[s_obs], S_star(np.log(np.poly1d(b[::-1])(i[s_obs]))+(v)/c), 'w lp ps 0.
 # compare the wavelength solutions
 show_model(i, np.poly1d(b[::-1])(i), np.poly1d(bg[::-1])(i), res=True)
 
-# fit a and b simulatenously
+# fit v, a and b simulatenously
 
 S_vab = lambda x, v, a, b0,b1,b2,b3: S_mod(x, v, [a], [b0,b1,b2,b3], 2.2)
-p, e_p = curve_fit(S_vab, i[s_obs], f_i[s_obs], p0=[v, 1, *bg])
+p_vab, e_p = curve_fit(S_vab, i[s_obs], f_i[s_obs], p0=[v, 1, *bg])
 show_model(i[s_obs], f_i[s_obs], S_vab(i[s_obs], *p))
 p1 = 1*p
 
+s_obs = slice(400,1800) # probbably the wavelength solution of the template is bad
+
+S_va = lambda x, v, a, b0,b1,b2,b3: S_mod(x, v, [a], [b0,b1,b2,b3], 2.2)
+p_va, e_p = curve_fit(S_va, i[s_obs], f_i[s_obs], p0=[v, 1,*p_vab[2:6]])
+p_va[0], np.diag(e_p)[0]**0.5
+show_model(i[s_obs], f_i[s_obs], S_va(i[s_obs], *p_va))
+
+
+
 S_vabs = lambda x, v, a, b0,b1,b2,b3, s: S_mod(x, v, [a], [b0,b1,b2,b3], s)
-p, e_p = curve_fit(S_vabs, i[s_obs], f_i[s_obs], p0=[*p1, 2.2])
-show_model(i[s_obs], f_i[s_obs], S_vabs(i[s_obs], *p))
+p_vabs, e_p_vabs = curve_fit(S_vabs, i[s_obs], f_i[s_obs], p0=[*p_va, 2.2])
+p_vabs[0], np.diag(e_p_vabs)[0]**0.5
+show_model(i[s_obs], f_i[s_obs], S_vabs(i[s_obs], *p_vabs))
 
 
 #show_model(i[s_obs], f_i[s_obs], S_b(i[s_obs], *bg))
