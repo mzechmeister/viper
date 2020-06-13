0b8d6e7c9701f83dc4a55a65ed839670f36bb268
Mathias Zechmeister <zechmeister@astro.physik.uni-goettingen.de>
2020-06-14 01:09:01 +0200

    refactor: Direct full fit, skip sequential fits
    Processing time (24 spectra, o:18-29) reduced from 20 min to 9m20s.
    A velocity start guess off by 12 km/s was working before, but now failed for some orders (o=30).
    Setting to 0 km/s, it works.

--- viper.py
+++ viper.py
@@ -16,6 +16,7 @@ from astropy.io import fits
 
 from gplot import *
 gplot.tmp = '$'
+gplot.colors('classic')
 from pause import pause
 
 from inst.inst_TLS import Spectrum, Tpl, FTS
@@ -27,7 +28,7 @@ c = 3e5   # [km/s] speed of light
 o = 18; lmin = 5240; lmax = 5390
 
 dirname = r''
-v0 = -12
+vg = 0
 ftsname = dirname + 'lib/TLS/FTS/TLS_I2_FTS.fits'
 obsname = dirname + 'data/TLS/betgem/BETA_GEM.fits'
 tplname = dirname + 'data/TLS/betgem/pepsib.20150409.000.sxt.awl.all6'
@@ -78,6 +79,10 @@ def fit_chunk(o, obsname):
     ####  data TLS  ####
     w_i, f_i, bp, bjd, berv = Spectrum(obsname, o=o)
     i = np.arange(f_i.size)
+    #s_obs = slice(400,1700) # probably the wavelength solution of the template is bad
+    mskatm = interp1d(*np.genfromtxt('lib/mask_vis1.0.dat').T)
+    bp[mskatm(w_i) > 0.1] |= 16
+    s_obs, = np.where(bp==0)
 
     ####  stellar template  ####
     w_tpl, f_tpl = Tpl(tplname, o=o)
@@ -90,8 +95,11 @@ def fit_chunk(o, obsname):
     s = slice(*np.searchsorted(w_I2, [lmin, lmax]))
     s_s = slice(*np.searchsorted(w_tpl, [lmin, lmax]))
 
+    #pause()
     if 0:
+        # plot data, template, and iodine without any modifications
         gplot(w_I2[s], f_I2[s], 'w l lc 9,', w_tpl[s_s], f_tpl[s_s], 'w l lc 3,', w_i, f_i, 'w lp lc 1 pt 7 ps 0.5')
+        # plot with some scaling
         #gplot(w_I2[s], f_I2[s]/1.18, 'w l lc 9,', w_tpl[s_s]*(1+12/c), f_tpl[s_s], 'w l lc 3,', w_i, f_i/1.04, 'w lp lc 1 pt 7 ps 0.5')
 
     # prepare input; convert discrete data to model
@@ -111,94 +119,79 @@ def fit_chunk(o, obsname):
     # setup the model
     S_mod = model(S_star, xj, iod_j, IP)
 
-    # plot the IP
-    gplot(S_mod.vk, S_mod.IP(S_mod.vk))
+    if 0:
+        # plot the IP
+        gplot(S_mod.vk, S_mod.IP(S_mod.vk))
 
     if 0:
        # plot again, now the stellar template can be interpolated
        gplot(np.exp(xj), iod_j, S_star(xj), 'w l lc 9, "" us 1:3 w l lc 3')
 
-    #gplot(np.exp(xj), iod_j, S_star(xj+3/c), 'w l lc 9 t "iodine", "" us 1:3 w l lc 3 t "template + 3 km/s",', np.exp(xj_eff), S_eff(xj_eff), 'w l lc 1 t "IP x (tpl*I2)"')
 
     # Now wavelength solution
 
     # mapping between pixel and wavelength
 
-    #lam(x) = b0 + b_1 * x + b_2 * x**2
+    #lam(x) = b0 + b1 * x + b2 * x^2
     lam = np.poly1d([w_i[0], (w_i[-1]-w_i[0])/w_i.size][::-1])
 
     # trim the observation to a range valid for the model
     s_obs = slice(*np.searchsorted(np.log(w_i), [xj[0]+100/c, xj[-1]-100/c]))
-    s_obs = np.r_[s_obs][np.where(bp[s_obs]==0)]
-
-    # well, we see the wavelength solution can be improved
-    #gplot(i[s_obs], S_eff(np.log(lam(i[s_obs]))), 'w l,', i, f_i, 'w lp pt 7 ps 0.5 lc 3')
-
-    # and let's plot the observation against wavelength
-
-    #gplot(np.exp(xj_eff), S_eff(xj_eff), 'w l,', lam(i), f_i, 'w lp pt 7 ps 0.5 lc 3')
+    s_obs = np.r_[s_obs][bp[s_obs]==0]
 
     # a parameter set
-    v = 0
-    a = [0.96]
-    b = [w_i[0], (w_i[-1]-w_i[0])/w_i.size] # [6128.8833940969, 0.05453566108124]
-    s = 2.5
-
-    # a simple call to the forward model
-    Si_mod = S_mod(i[s_obs], v=0, a=[1], b=b, s=s)
-
-    #gplot(i, Si_mod, 'w l t "S(i)",', i, f_i, 'w lp pt 7 ps 0.5 lc 3 t "S_i"')
-    #show_model(i[s_obs], f_i[s_obs], Si_mod, res=False)
-    #pause()
-
-    # A wrapper to fit the continuum
-    S_a = lambda x, a0: S_mod(x, v, [a0], b, s)
-    a, e_a = curve_fit(S_a, i[s_obs], f_i[s_obs])
-    #show_model(i[s_obs], f_i[s_obs], S_a(i[s_obs],*a), res=False)
-
-    # A wrapper to fit the wavelength solution
-    S_b = lambda x, b0,b1,b2,b3: S_mod(x, v, a, [b0,b1,b2,b3], s)
-
-    v = v0   # a good guess for the stellar RV is needed
-    bg = np.polyfit(i[s_obs], w_i[s_obs], 3)[::-1]
+    v = vg   # a good guess for the stellar RV is needed
+    a = ag = [np.mean(f_i) / np.mean(S_star(np.log(w_i[s_obs])))] 
+    b = bg = [w_i[0], (w_i[-1]-w_i[0])/w_i.size] # [6128.8833940969, 0.05453566108124]
+    b = bg = np.polyfit(i[s_obs], w_i[s_obs], 3)[::-1]
     #show_model(i[s_obs], f_i[s_obs], S_b(i[s_obs],*bg), res=False)
-    b, e_b = curve_fit(S_b, i[s_obs], f_i[s_obs], p0=bg)
-    bg1 = b*1
+    s = sg = 2.5
 
-    #show_model(i[s_obs], f_i[s_obs], S_b(i[s_obs], *bg))
-    #show_model(i[s_obs], f_i[s_obs], S_b(i[s_obs], *b))
-    #gplot+(i[s_obs], S_star(np.log(np.poly1d(b[::-1])(i[s_obs]))+(v)/c), 'w lp ps 0.5')
+    if 0:
+        # a simple call to the forward model
+        # Si_mod = S_mod(i[s_obs], v=0, a=a, b=b, s=s)
+        # show the start guess
+        S_mod.show([v,a,b,s], i[s_obs], f_i[s_obs], res=False, dx=0.1)
+        #pause()
+
+    if 0:
+        # A wrapper to fit the continuum
+        S_a = lambda x, a0: S_mod(x, v, [a0], b, s)
+        a, e_a = curve_fit(S_a, i[s_obs], f_i[s_obs])
+        #show_model(i[s_obs], f_i[s_obs], S_a(i[s_obs],*a), res=False)
+
+    if 0:
+        # A wrapper to fit the wavelength solution
+        S_b = lambda x, b0,b1,b2,b3: S_mod(x, v, a, [b0,b1,b2,b3], s)
+        b, e_b = curve_fit(S_b, i[s_obs], f_i[s_obs], p0=bg)
+        #show_model(i[s_obs], f_i[s_obs], S_b(i[s_obs], *bg))
+        #show_model(i[s_obs], f_i[s_obs], S_b(i[s_obs], *b))
+        #gplot+(i[s_obs], S_star(np.log(np.poly1d(b[::-1])(i[s_obs]))+(v)/c), 'w lp ps 0.5')
+
 
     # compare the wavelength solutions
     #show_model(i, np.poly1d(b[::-1])(i), np.poly1d(bg[::-1])(i), res=True)
+    #gplot(i, (np.poly1d(b[::-1])(i)/np.poly1d(bg[::-1])(i)-1)*c)
 
-    # fit v, a and b simulatenously
+    if 0:
+        # fit v, a and b simulatenously
+        S_vab = lambda x, v, a, b0,b1,b2,b3: S_mod(x, v, [a], [b0,b1,b2,b3], 2.2)
+        p_vab, e_p = curve_fit(S_vab, i[s_obs], f_i[s_obs], p0=[v, 1, *bg])
+        show_model(i[s_obs], f_i[s_obs], S_vab(i[s_obs], *p_vab))
 
-    S_vab = lambda x, v, a, b0,b1,b2,b3: S_mod(x, v, [a], [b0,b1,b2,b3], 2.2)
-    p_vab, e_p = curve_fit(S_vab, i[s_obs], f_i[s_obs], p0=[v, 1, *bg])
-    show_model(i[s_obs], f_i[s_obs], S_vab(i[s_obs], *p_vab))
-
-    #s_obs = slice(400,1700) # probably the wavelength solution of the template is bad
-    mskatm = interp1d(*np.genfromtxt('lib/mask_vis1.0.dat').T)
-    bp[mskatm(w_i) > 0.1] |= 16
-    s_obs, = np.where(bp==0)
-
-    S_va = lambda x, v, a, b0,b1,b2,b3: S_mod(x, v, [a], [b0,b1,b2,b3], 2.2)
-    p_va, e_p = curve_fit(S_va, i[s_obs], f_i[s_obs], p0=[v, 1,*p_vab[2:6]])
-    p_va[0], np.diag(e_p)[0]**0.5
-    #show_model(i[s_obs], f_i[s_obs], S_va(i[s_obs], *p_va))
-
-    S_vabs = lambda x, v, a, b0,b1,b2,b3, s: S_mod(x, v, [a], [b0,b1,b2,b3], s)
-    p_vabs, e_p_vabs = curve_fit(S_vabs, i[s_obs], f_i[s_obs], p0=[*p_va, 2.2], epsfcn=1e-12)
-    rvo, e_rvo = p_vabs[0], np.diag(e_p_vabs)[0]**0.5
-    #print(o, rvo, e_rvo)
-    show_model(i[s_obs], f_i[s_obs], S_vabs(i[s_obs], *p_vabs))
+    if 0:
+        # fit v, a and b simulatenously
+        S_vabs = lambda x, v, a, b0,b1,b2,b3, s: S_mod(x, v, [a], [b0,b1,b2,b3], s)
+        p_vabs, e_p_vabs = curve_fit(S_vabs, i[s_obs], f_i[s_obs], p0=[*p_va, 2.2], epsfcn=1e-12)
+        #rvo, e_rvo = p_vabs[0], np.diag(e_p_vabs)[0]**0.5
+        #print(o, rvo, e_rvo)
+        show_model(i[s_obs], f_i[s_obs], S_vabs(i[s_obs], *p_vabs))
 
     S = lambda x, v, a0,a1,a2,a3, b0,b1,b2,b3, s: S_mod(x, v, [a0,a1,a2,a3], [b0,b1,b2,b3], s)
-    p_vabs, e_p_vabs = curve_fit(S, i[s_obs], f_i[s_obs], p0=[*p_vabs[:2]]+[0]*3+[*p_vabs[2:]], epsfcn=1e-12)
-    rvo, e_rvo = 1000*p_vabs[0], 1000*np.diag(e_p_vabs)[0]**0.5   # convert to m/s
+    p, e_p = curve_fit(S, i[s_obs], f_i[s_obs], p0=[v]+a+[0]*3+[*bg, s], epsfcn=1e-12)
+    rvo, e_rvo = 1000*p[0], 1000*np.diag(e_p)[0]**0.5   # convert to m/s
     #show_model(i[s_obs], f_i[s_obs], S(i[s_obs], *p_vabs))
-    S_mod.show([*p_vabs[:1],p_vabs[1:5],p_vabs[5:9], p_vabs[9]], i[s_obs], f_i[s_obs], dx=0.1)
+    S_mod.show([p[0], p[1:5], p[5:9], p[9]], i[s_obs], f_i[s_obs], dx=0.1)
 
     #show_model(i[s_obs], f_i[s_obs], S_b(i[s_obs], *bg))
     #show_model(i[s_obs], f_i[s_obs], S_vabs(i[s_obs], *p))
@@ -227,7 +220,7 @@ for n,obsname in enumerate(obsnames):
         except Exception as e:
             if repr(e) == 'BdbQuit()':
                exit()
-            print(repr(e))
+            print("Order failed due to:", repr(e))
 
         print(n+1, o, rv[i_o], e_rv[i_o])
 
