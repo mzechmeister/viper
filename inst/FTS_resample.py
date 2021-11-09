c32f1eda7d5ac1e5485a4f63e34fc8e57634d703
Jana Koehler <jana@tls-tautenburg.de>
2021-11-09 10:05:04 +0100

    Update read in CRIRES data

--- inst/FTS_resample.py
+++ inst/FTS_resample.py
@@ -6,16 +6,24 @@ c = 3e8   # [m/s] speed of light
 
 
 def FTSfits(ftsname):
-    hdu = fits.open(ftsname, ignore_blank=True, output_verify='silentfix')[0]
 
-    f = hdu.data[::-1]
-    h = hdu.header
-    try:
-        w = h['CRVAL1'] + h['CDELT1'] * (np.arange(f.size) + 1. - h['CRPIX1'])
-    except:
-        # for OES
-        w = h['CRVAL1'] + h['CDELT1'] * (np.arange(f.size) + 1.)
-    w = 1e8 / w[::-1]   # convert wavenumbers to wavelength [angstrom]
+    if ftsname.endswith(".dat"):
+        data = np.loadtxt(ftsname)
+        w = data[:,0]
+        f = data[:,1]
+        f = f[::-1]
+        w = 1e8 / w[::-1]
+    else:
+        hdu = fits.open(ftsname, ignore_blank=True, output_verify='silentfix')[0]
+
+        f = hdu.data[::-1]
+        h = hdu.header
+        try:
+            w = h['CRVAL1'] + h['CDELT1'] * (np.arange(f.size) + 1. - h['CRPIX1'])
+        except:
+            # for OES
+            w = h['CRVAL1'] + h['CDELT1'] * (np.arange(f.size) + 1.)
+        w = 1e8 / w[::-1]   # convert wavenumbers to wavelength [angstrom]
 
     return w, f
 
