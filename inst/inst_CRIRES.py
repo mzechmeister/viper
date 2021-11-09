63c25b85bb6d667c419c927866ea23f8bb9a8612
Jana Koehler <jana@tls-tautenburg.de>
2021-11-09 10:04:57 +0100

    Update read in CRIRES data

--- inst/inst_CRIRES.py
+++ inst/inst_CRIRES.py
@@ -35,8 +35,6 @@ def Spectrum(filename='', o=None, targ=None):
     hdr = hdu[1].header
     exptime = hdr['EXPTIME']
 
-    print('---dateobs, exptime  :', dateobs, exptime)
-
     targdrs = SkyCoord(ra=ra*u.deg, dec=de*u.deg)
     if not targ: targ = targdrs
     midtime = Time(dateobs, format='isot', scale='utc') + exptime * u.s
@@ -47,55 +45,46 @@ def Spectrum(filename='', o=None, targ=None):
     oi = 5-int((o-1)/3)  
     d = np.mod(o,3) 
     if d == 0:
-        d=3
+        d = 3
 
-    w = (hdu[d].data.field(3*oi+2))*10
     e = err = hdu[d].data.field(3*oi+1)
     f = hdu[d].data.field(3*oi)
     x = np.arange(f.size) 
 
-    if (o >= 10) and (o <= 16):
-       # using an own wavelength solution as the one given by CRIRES+ pipeline is wrong
-        bw = (np.load('wave_solution_cell_best.npy'))[o-1]
+    setting = (hdu[0]).header['ESO INS WLEN ID']
+    # using an own wavelength solution as the one given by CRIRES+ pipeline is wrong
+    if setting == 'K2166': 
+        bw = (np.load('lib/CRIRES/wavesolution/wave_solution_K2166.npy'))[o-1]   
+        w = np.poly1d(bw[::-1])(x)  
+    elif setting == 'K2192':  
+        bw = (np.load('lib/CRIRES/wavesolution/wave_solution_K2192.npy'))[o-1]
         w = np.poly1d(bw[::-1])(x)
     else:
-       # still a better wavelength solution for other orders needed
-       # should be done with more data in next commisioning run (May 2021)
-        obsna = 'data/CRIRES/210219_Ross619/withSGC/cr2res_obs_nodding_extracted_combined.fits'
-        hdu = fits.open(filename, ignore_blank=True)
         w = (hdu[d].data.field(3*oi+2))*10
-        shift_cell = [111.6,110.2,80.76,88.12,70,120,138,82.1,81.02,80.98,51.67, 81.78,102.48, 66.35,98.11,92.08,64.73,110.06]       
-        w /= (1+shift_cell[o-1]/3e5)
- 
 
-    # correction of instrumental shift
-    # applying a offset correction for wavelength solution
-    # is it needed? is it right?
- #   sss = [ 2.59857143-0.17,  2.80714286-0.22,  3.21571429-0.2,  3.03285714+0.58,  3.64428571-0.26,  3.45857143, 1.90571429+0.37]
- #   sss = [1.944675, 2.206255, 2.5936, 3.1737325, 3.14364, 3.1717725, 2.3365325]
-  #  w /= (1+(sss[o-10]+0.2)/3e5)
+    try:
+        mtrlgy = (hdu[0]).header['ESO OCS MTRLGY DX']
+        f = np.interp(x+mtrlgy,x,f)
+    except:
+        pass
 
-    print("---berv, wlmin, wlmax:", berv, w[0],w[-1])
-    
-    fwhm = (hdu[d]).header['ESO QC SLITFWHM'+str(oi+2)]
-    snr = (hdu[d]).header['ESO QC SNR'+str(oi+2)]
-    slit = (hdu[0]).header['ESO INS SLIT1 WID']
-
-    print("---fwhm, snr, slit   :", fwhm, snr, slit)
-
-#    w = airtovac(w)	# still needed with own ws?
-    
     b = 1 * np.isnan(f) # bad pixel map
- #   b[f>15000] |= 2 # large flux
+
+    # using flag file for noisy regions
+    A = np.genfromtxt('lib/CRIRES/flag_file.dat', dtype=None, names=True).view(np.recarray)
+    flag_start = A.ok_s
+    flag_end = A.ok_e
+
+    b[:flag_start[o-1]] |= 64
+    b[flag_end[o-1]:] |= 64
 
     return x, w, f, b, bjd, berv
 
 def Tpl(tplname, o=None, targ=None):
     '''Tpl should return barycentric corrected wavelengths'''
-    if tplname.endswith('.model'):
-        # echelle template
-        x, w, f, b, bjd, berv = Spectrum(tplname, o=o, targ=targ)
-        w *= 1 + (berv*u.km/u.s/c).to_value('')   # *model already barycentric corrected (?)
+    if tplname.endswith('.npy'):
+        w = np.load(tplname)[o,0]
+        f = np.load(tplname)[o,1]
     else:
         # long 1d template
         x, w, f, b, bjd, berv = Spectrum(tplname, o=o, targ=targ)   
