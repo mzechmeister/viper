89345a5a2db210a1623fd1dd02dabfab7f291b40
Jana Köhler <77435060+JanaKoehler1985@users.noreply.github.com>
2023-03-09 15:13:01 +0100

    chore: Responsive GUI, Updated Layout
    New:
    - larger entry for data, tpl and flag file
    - Responsive GUI in x axis (using grid)
    
    
    Coming next:
    - search button flagfile
    - checkboxes molecules
    - extended nset, oset entry
    - entry for look, lookfast
    - print console command
    - read in FTS Cell -> remove ocen
    - popup help icons
    
    (- updating layout of vpr.py)

--- GUI_viper.py
+++ GUI_viper.py
@@ -94,6 +94,14 @@ def bt_tpl():
 def bt_exit():
     exit()
 
+def new(event):
+    win_width = win.winfo_width()
+    win_height = event.height
+    fr1.config(width=win_width-40)
+    fr2.config(width=(win_width-46)/2)
+    fr3.config(width=(win_width-46)/2)
+    fr4.config(width=(win_width-46)/2)
+
 ###### SETTING ######
 
 font_type = 'Arial'
@@ -101,8 +109,11 @@ font_size = 12
 bg_frame = '#f1f1f1'    # bg color big frame
 bg_color = '#e6e1e1'    # bg color small frames
 
-win_width = 840     # width of GUI window
-win_high = 630      # height of GUI window
+win_width = 940     # width of GUI window	# 840
+win_high = 690      # height of GUI window	# 630
+xy0 = 20
+y1 = 5
+x1 = 10
 
 win = Tk()
 win.title('Gui VIPER')
@@ -110,214 +121,224 @@ win.geometry("{}x{}".format(win_width, win_high))
 win.configure(background=bg_color)
 
 
-###### POSITIONS ######
-
-x0 = 40     # positions start left
-lh = 30     # line height
-xs = 100
-xss = 80
-sp = 210
-sps = 170
-sl = 7
-
 ###### FRAMES #######
+win.bind("<Configure>", new)
+win.columnconfigure(0, weight=1)
+win.columnconfigure(3, weight=1)
+#win.rowconfigure(0, weight=1)
 
 # options: solid, sunken, ridge, groove, raised
 # groove, ridge need bd=2 or more
-Frame(master=win, height=138, width=win_width-40, bg=bg_frame, bd=2, relief='groove').place(x=20, y=20)
+fr1 = Frame(win, height=148, width=win_width-40, bg=bg_frame, bd=2, relief='groove')
+fr1.grid(row=0, column = 1, sticky="nw",padx=20,pady=(xy0,6), columnspan=2)
+fr1.grid_propagate(False)
+fr1.grid_columnconfigure(2, weight=1)
+fr1.grid_columnconfigure(4, weight=1)
+fr1.grid_rowconfigure(0, weight=1)
+fr1.grid_rowconfigure(5, weight=1)
 
-Frame(master=win, height=378, width=380, bg=bg_frame, bd=2, relief='groove').place(x=20, y=50+112)
-Frame(master=win, height=283, width=414, bg=bg_frame, bd=2, relief='groove').place(x=405, y=50+112)
-Frame(master=win, height=115, width=414, bg=bg_frame, bd=2, relief='groove').place(x=405, y=50+112+288)
+fr2 = Frame(master=win, height=428, width=(win_width-46)/2, bg=bg_frame, bd=2, relief='groove')
+fr2.grid(row=1, column = 1, sticky="nw",padx=(xy0,6),pady=0, rowspan=2)
+fr2.grid_propagate(False)
+fr2.grid_columnconfigure(1, weight=1)
+fr2.grid_columnconfigure(3, weight=1)
+#fr2.grid_rowconfigure(0, weight=1)
+
+fr3 = Frame(master=win, height=303, width=(win_width-46)/2, bg=bg_frame, bd=2, relief='groove')
+fr3.grid(row=1, column = 2, sticky="nw",padx=(0, xy0),pady=0)
+fr3.grid_propagate(False)
+fr3.grid_columnconfigure(0, weight=1)
+fr3.grid_columnconfigure(1, weight=1)
+
+fr4 = Frame(master=win, height=135, width=(win_width-46)/2, bg=bg_frame, bd=2, relief='groove')
+fr4.grid(row=2, column = 2, sticky="nw",padx=(0, xy0),pady=(6,0))
+fr4.grid_propagate(False)
 
 ###### BUTTONS ######
 
 ttk.Style().configure("TButton", padding=2,   background="#fdfdfd", font=(font_type,font_size,'bold'), borderwidth =2)
 
 b_exit = ttk.Button(master=win, text='EXIT', command = bt_exit)
-b_exit.place(x=win_width-82,y=win_high-50, width=60)
+b_exit.grid(row=3, column=2, sticky="ne", padx=(0,xy0), pady = xy0)
 
 b_go = ttk.Button(master=win, text='Start', command = bt_start)
-b_go.place(x=win_width-162, y=win_high-50, width=60)
+b_go.grid(row=3, column=2, sticky="ne", padx=(0,130), pady = xy0)
 
-b_tpl = Button(master=win, text='Search data file', command = bt_data, background="#fdfdfd")
-b_tpl.place(x=win_width-580, y=lh, height=25)
+b_dat = Button(fr1, text='Search data file', command = bt_data, background="#fdfdfd", width=15)
+b_dat.grid(row=1, column=7, sticky="nw", padx=xy0)
 
-b_tpl = Button(master=win,text='Search tpl file', command = bt_tpl, background="#fdfdfd")
-b_tpl.place(x=win_width-168, y=lh, height=25)
+b_tpl = Button(fr1,text='Search tpl file', command = bt_tpl, background="#fdfdfd", width=15)
+b_tpl.grid(row=2, column=7, sticky="nw", padx=xy0)
 
 ###### ENTRIES ######
 
-e_dat = Entry(master=win)
+e_dat = Entry(fr1, width = 200)
 e_dat.insert(0, 'data/TLS/hd189733/*')
-e_dat.place(x=x0, y=2*lh, width=350)
+e_dat.grid(row=1, column=1, sticky="nw", padx=x1, pady=y1, columnspan=6)
 
-e_tpl = Entry(master=win)
+e_tpl = Entry(fr1, width = 200)
 e_tpl.insert(0, 'data/TLS/Deconv/HARPS*fits')
-e_tpl.place(x=x0+400, y=2*lh, width=350)
+e_tpl.grid(row=2, column=1, sticky="nw", padx=x1, pady=y1, columnspan=6)
 
-e_flag = Entry(master=win)
+e_flag = Entry(fr1, width = 200)
 e_flag.insert(0, '')
-e_flag.place(x=x0+xs, y=4*lh, width=340)
+e_flag.grid(row=3, column=1, sticky="nw", padx=x1, pady=y1, columnspan=6)
 
-e_targ = Entry(master=win)
-e_targ.place(x=x0+sp+xss, y=3*lh, width=150)
+e_targ = Entry(fr1, width=70)
+e_targ.grid(row=4, column=4, sticky="nw", padx=x1, pady=y1)
 
-e_tag = Entry(master=win)
-e_tag.place(x=x0+2*sp+xss, y=3*lh, width=240)
+e_tag = Entry(fr1, width=35)
+e_tag.grid(row=4, column=6, sticky="nw", padx=xy0, pady=y1, columnspan=2)
 
-e_nset = Entry(master=win)
+e_nset = Entry(fr2)#, width=15)
 e_nset.insert(0, ':1')
-e_nset.place(x=x0+xss, y=(sl+0)*lh, width=100)
+e_nset.grid(row=1, column=1, sticky="nw", padx=(x1,xy0), pady=y1)
 
-e_oset = Entry(master=win)
+e_oset = Entry(fr2)
 e_oset.insert(0, '20')
-e_oset.place(x=x0+xss, y=(sl+1)*lh, width=100)
+e_oset.grid(row=2, column=1, sticky="nw", padx=(x1,xy0), pady=y1)
 
-e_ch = Entry(master=win)
+e_ch = Entry(fr2)
 e_ch.insert(0, '1')
-e_ch.place(x=x0+xss, y=(sl+2)*lh, width=60)
+e_ch.grid(row=3, column=1, sticky="nw", padx=(x1,xy0), pady=y1)
 
-e_deg_norm = Entry(master=win)
+e_deg_norm = Entry(fr2)
 e_deg_norm.insert(0, '3')
-e_deg_norm.place(x=x0+xss+1*sp, y=(sl+4)*lh, width=50)
+e_deg_norm.grid(row=5, column=3, sticky="nw", padx=(x1,xy0), pady=(y1,0))
 
-e_deg_wave = Entry(master=win)
+e_deg_wave = Entry(fr2)
 e_deg_wave.insert(0, '3')
-e_deg_wave.place(x=x0+xss+1*sp, y=(sl+5)*lh, width=50)
+e_deg_wave.grid(row=6, column=3, sticky="nw", padx=(x1,xy0), pady=(y1,0))
 
-e_deg_bkg = Entry(master=win)
+e_deg_bkg = Entry(fr2)
 e_deg_bkg.insert(0, '1')
-e_deg_bkg.place(x=x0+xss+1*sp, y=(sl+6)*lh, width=50)
+e_deg_bkg.grid(row=7, column=3, sticky="nw", padx=(x1,xy0), pady=(y1,0))
 
-e_tsig = Entry(master=win)
+e_tsig = Entry(fr2)
 e_tsig.insert(0, '1')
-e_tsig.place(x=x0+xss+0*sp, y=(sl+7)*lh, width=50)
+e_tsig.grid(row=8, column=1, sticky="nw", padx=(x1,xy0), pady=y1)
 
-e_iphs = Entry(master=win)
+e_iphs = Entry(fr2)
 e_iphs.insert(0, '50')
-e_iphs.place(x=x0+xss+sp, y=(sl+1)*lh, width=50)
+e_iphs.grid(row=2, column=3, sticky="nw", padx=(x1,xy0), pady=(y1,0))
 
-e_vg = Entry(master=win)
+e_vg = Entry(fr2)
 e_vg.insert(0, '1')
-e_vg.place(x=x0+xss+sp, y=(sl+2)*lh, width=50)
+e_vg.grid(row=3, column=3, sticky="nw", padx=(x1,xy0), pady=(y1,0))
 
-e_vcut = Entry(master=win)
+e_vcut = Entry(fr2)
 e_vcut.insert(0, '100')
-e_vcut.place(x=x0+xss+sp, y=(sl+3)*lh, width=50)
+e_vcut.grid(row=4, column=3, sticky="nw", padx=(x1,xy0), pady=(y1,0))
 
-e_kapsig = Entry(master=win)
+e_kapsig = Entry(fr2)
 e_kapsig.insert(0, '4.5')
-e_kapsig.place(x=x0+xss+0*sp, y=(sl+3)*lh, width=60)
+e_kapsig.grid(row=4, column=1, sticky="nw", padx=(x1,xy0), pady=y1)
 
-e_overs = Entry(master=win)
+e_overs = Entry(fr2)
 e_overs.insert(0, '1')
-e_overs.place(x=x0+xss+0*sp, y=(sl+4)*lh, width=60)
+e_overs.grid(row=5, column=1, sticky="nw", padx=(x1,xy0), pady=y1)
 
-e_molec = Entry(master=win)
+e_molec = Entry(fr4)
 e_molec.insert(0, 'all')
-e_molec.place(x=x0+380, y=(sl+10.5)*lh, width=250)
+e_molec.grid(row=2, column=0, sticky="nw", padx=(xy0,0), pady=y1, columnspan=3)
 
 ###### COMBOBOXES ######
 
-combo_inst = ttk.Combobox(master=win, values=['TLS', 'CRIRES','cplCRIRES', 'CES', 'KECK', 'UVES', 'OES'])
+combo_inst = ttk.Combobox(fr1, values=['TLS', 'CRIRES','cplCRIRES', 'CES', 'KECK', 'UVES', 'OES'], width=10)
 combo_inst.set('TLS')
-combo_inst.place(x=x0+xs, y=3*lh, width=100)
+combo_inst.grid(row=4, column=1, sticky="nw", padx=x1, pady=y1)
 
-combo_ip = ttk.Combobox(master=win, values=['g', 'ag', 'agr', 'bg', 'mcg', 'mg', 'sg', 'bnd'])
+combo_ip = ttk.Combobox(fr2, values=['g', 'ag', 'agr', 'bg', 'mcg', 'mg', 'sg', 'bnd'])
 combo_ip.set('g')
-combo_ip.place(x=x0+xss+sp, y=(sl+0)*lh, width=50)
+combo_ip.grid(row=1, column=3, sticky="nw", padx=(x1,xy0), pady=y1)
 
-combo_stepRV = ttk.Combobox(master=win, values=['', 'a', 'm'])
+combo_stepRV = ttk.Combobox(fr2, values=['', 'a', 'm'])
 combo_stepRV.set('')
-combo_stepRV.place(x=x0+xss+0*sp, y=(sl+5)*lh, width=60)
+combo_stepRV.grid(row=6, column=1, sticky="nw", padx=(x1,xy0), pady=y1)
 
-combo_tell = ttk.Combobox(master=win, values=['', 'add', 'add2', 'mask', 'sig'])
+combo_tell = ttk.Combobox(fr2, values=['', 'add', 'add2', 'mask', 'sig'])
 combo_tell.set('')
-combo_tell.place(x=x0+xss+0*sp, y=(sl+6)*lh, width=60)
+combo_tell.grid(row=7, column=1, sticky="nw", padx=(x1,xy0), pady=y1)
 
 ###### CHECKBOXES ######
 
 ttk.Style().configure("TCheckbutton", background=bg_frame, bd=0, highlightthickness=0)
 
 cb_flagfile = IntVar()
-ttk.Checkbutton(master=win, text=" ", variable=cb_flagfile).place(x=x0, y=4*lh)
+ttk.Checkbutton(fr1, text="   flag file:", variable=cb_flagfile).grid(row=3, column=0, sticky="nw", padx=x1, pady=y1)
 
 cb_wgt = IntVar()
-ttk.Checkbutton(master=win, text="     weighted error", variable=cb_wgt).place(x=x0+sp+0*sps, y=(sl+7)*lh)
+ttk.Checkbutton(fr2, text="     weighted error", variable=cb_wgt).grid(row=8, column=2, sticky="nw", padx=(xy0,x1), pady=y1, columnspan=2)
 
 cb_nocell = IntVar()
-ttk.Checkbutton(master=win, text="     no cell", variable=cb_nocell).place(x=x0+sp+0*sps, y=(sl+9)*lh)
+ttk.Checkbutton(fr2, text="     no cell", variable=cb_nocell).grid(row=x1, column=2, sticky="nw", padx=(xy0,x1), pady=y1)
 cb_createtpl = IntVar()
-ttk.Checkbutton(master=win, text="     create tpl", variable=cb_createtpl).place(x=x0, y=(sl+9)*lh)
+ttk.Checkbutton(fr2, text="     create tpl", variable=cb_createtpl).grid(row=x1, column=0, sticky="nw", padx=(xy0,x1), pady=y1)
 cb_tellshift = IntVar()
-ttk.Checkbutton(master=win, text="     tell shift", variable=cb_tellshift).place(x=x0, y=(sl+10)*lh)
+ttk.Checkbutton(fr2, text="     tell shift", variable=cb_tellshift).grid(row=11, column=0, sticky="nw", padx=(xy0,x1), pady=y1)
 
 cb_demo = [IntVar(), IntVar(), IntVar(), IntVar(), IntVar(), IntVar(), IntVar()]
-ttk.Checkbutton(master=win, text="     raw data", variable=cb_demo[0]).place(x=x0+sp+sps, y=(sl+0)*lh)
-ttk.Checkbutton(master=win, text="     plot IP", variable=cb_demo[1]).place(x=x0+sp+sps, y=(sl+1)*lh)
-ttk.Checkbutton(master=win, text="     stellar tpl", variable=cb_demo[2]).place(x=x0+sp+sps, y=(sl+2)*lh)
-ttk.Checkbutton(master=win, text="     forward model", variable=cb_demo[3]).place(x=x0+sp+sps, y=(sl+3)*lh)
-ttk.Checkbutton(master=win, text="     fit continuum", variable=cb_demo[4]).place(x=x0+sp+2*sps, y=(sl+0)*lh)
-ttk.Checkbutton(master=win, text="     wavelength solution", variable=cb_demo[5]).place(x=x0+sp+2*sps, y=(sl+1)*lh)
-ttk.Checkbutton(master=win, text="     fit vguess", variable=cb_demo[6]).place(x=x0+sp+2*sps, y=(sl+2)*lh)
+ttk.Checkbutton(fr3, text="     raw data", variable=cb_demo[0]).grid(row=1, column=0, sticky="nw", padx=(xy0,x1), pady=y1)
+
+ttk.Checkbutton(fr3, text="     plot IP", variable=cb_demo[1]).grid(row=2, column=0, sticky="nw", padx=(xy0,x1), pady=y1)
+ttk.Checkbutton(fr3, text="     stellar tpl", variable=cb_demo[2]).grid(row=3, column=0, sticky="nw", padx=(xy0,x1), pady=y1)
+ttk.Checkbutton(fr3, text="     forward model", variable=cb_demo[3]).grid(row=4, column=0, sticky="nw", padx=(xy0,x1), pady=y1)
+ttk.Checkbutton(fr3, text="     fit continuum", variable=cb_demo[4]).grid(row=1, column=1, sticky="nw", padx=(xy0,x1), pady=y1)
+ttk.Checkbutton(fr3, text="     wavelength solution", variable=cb_demo[5]).grid(row=2, column=1, sticky="nw", padx=(xy0,x1), pady=y1)
+ttk.Checkbutton(fr3, text="     fit vguess", variable=cb_demo[6]).grid(row=3, column=1, sticky="nw", padx=(xy0,x1), pady=y1)
 cb_lookguess = IntVar()
-ttk.Checkbutton(master=win, text="     lookguess", variable=cb_lookguess).place(x=x0+sp+2*sps, y=(sl+3)*lh)
+ttk.Checkbutton(fr3, text="     lookguess", variable=cb_lookguess).grid(row=4, column=1, sticky="nw", padx=(xy0,x1), pady=y1)
 cb_lookpar = IntVar()
-ttk.Checkbutton(master=win, text="     lookpar", variable=cb_lookpar).place(x=x0+sp+2*sps, y=(sl+4)*lh)
+ttk.Checkbutton(fr3, text="     lookpar", variable=cb_lookpar).grid(row=5, column=1, sticky="nw", padx=(xy0,x1), pady=y1)
 cb_lookres = IntVar()
-ttk.Checkbutton(master=win, text="     lookres", variable=cb_lookres).place(x=x0+sp+1*sps, y=(sl+4)*lh)
+ttk.Checkbutton(fr3, text="     lookres", variable=cb_lookres).grid(row=5, column=0, sticky="nw", padx=(xy0,x1), pady=y1)
 cb_infoprec = IntVar()
-ttk.Checkbutton(master=win, text="     infoprec", variable=cb_infoprec).place(x=x0+sp+1*sps, y=(sl+5)*lh)
+ttk.Checkbutton(fr3, text="     infoprec", variable=cb_infoprec).grid(row=6, column=0, sticky="nw", padx=(xy0,x1), pady=y1)
 cb_lookfast = IntVar()
-ttk.Checkbutton(master=win, text="     lookfast", variable=cb_lookfast).place(x=x0+sp+1*sps, y=(sl+7)*lh)
+ttk.Checkbutton(fr3, text="     lookfast", variable=cb_lookfast).grid(row=8, column=0, sticky="nw", padx=(xy0,x1), pady=y1)
 cb_lookfast.set(1)
 cb_look = IntVar()
-ttk.Checkbutton(master=win, text="     look", variable=cb_look).place(x=x0+sp+2*sps, y=(sl+7)*lh)
-
-
-#cb_atmmask = IntVar()
-#ttk.Checkbutton(master=win, text="     atmmask", variable=cb_atmmask).place(x=x0+0*sp, y=(sl+5)*lh)
-#cb_atmmod = IntVar()
-#ttk.Checkbutton(master=win, text="     atmmod", variable=cb_atmmod).place(x=x0+0*sp, y=(sl+6)*lh)
+ttk.Checkbutton(fr3, text="     look", variable=cb_look).grid(row=8, column=1, sticky="nw", padx=(xy0,x1), pady=y1)
 
 
 ###### LABELS ######
 
-Label(master=win, text='template file', font=(font_type, font_size, 'bold'), background=bg_frame).place(x=x0+400,y=lh)
-Label(master=win, text='data files', font=(font_type, font_size, 'bold'), background=bg_frame).place(x=x0, y=lh)
-Label(master=win, text='spectrograph:', background=bg_frame).place(x=x0, y=3*lh)
-Label(master=win, text='flag file:', background=bg_frame).place(x=x0+20, y=4*lh)
-Label(master=win, text='targ:', background=bg_frame).place(x=x0+sp+30, y=3*lh)
-Label(master=win, text='tag:', background=bg_frame).place(x=x0+2*sp+30, y=3*lh)
 
-Label(master=win, text='Options data reduction', font=(font_type, font_size, 'bold'), background=bg_frame).place(x=x0, y=(sl-1.2)*lh)
-Label(master=win, text='Options plotting data', font=(font_type, font_size, 'bold'), background=bg_frame).place(x=x0+sp+sps, y=(sl-1.2)*lh)
-Label(master=win, text='Advanced', font=(font_type, font_size, 'bold'), background=bg_frame).place(x=x0+0*sp, y=(sl+8)*lh)
-Label(master=win, text='Molecular specifies (for telluric add)', font=(font_type, font_size, 'bold'), background=bg_frame).place(x=x0+sp+sps, y=(sl+8.5)*lh)
-Label(master=win, text='Optical: H2O O2; NIR: H2O CH4 N2O CO2 CO', background=bg_frame).place(x=x0+sp+sps, y=(sl+9.5)*lh)
+Label(fr1, text='data files', font=(font_type, font_size, 'bold'), background=bg_frame).grid(row=1, column=0, sticky="nw", padx=x1, pady=y1)
+Label(fr1, text='template file', font=(font_type, font_size, 'bold'), background=bg_frame).grid(row=2, column=0, sticky="nw", padx=x1, pady=y1)
 
-Label(master=win, text='nset:', background=bg_frame).place(x=x0, y=(sl+0)*lh)
-Label(master=win, text='oset:', background=bg_frame).place(x=x0, y=(sl+1)*lh)
-Label(master=win, text='chunks:', background=bg_frame).place(x=x0, y=(sl+2)*lh)
+Label(fr1, text='spectrograph:', background=bg_frame).grid(row=4, column=0, sticky="nw", padx=x1, pady=y1)
 
-Label(master=win,text='IP:', background=bg_frame).place(x=x0+sp, y=(sl+0)*lh)
-Label(master=win,text='iphs:', background=bg_frame).place(x=x0+sp, y=(sl+1)*lh)
-Label(master=win,text='rvguess:', background=bg_frame).place(x=x0+sp, y=(sl+2)*lh)
-Label(master=win,text='vcut:', background=bg_frame).place(x=x0+1*sp, y=(sl+3)*lh)
+Label(fr1, text='targ:', background=bg_frame).grid(row=4, column=3, sticky="nw", padx=xy0, pady=y1)
+Label(fr1, text='tag:', background=bg_frame).grid(row=4, column=5, sticky="nw", padx=xy0, pady=y1)
 
-Label(master=win,text='deg_norm:', background=bg_frame).place(x=x0+1*sp, y=(sl+4)*lh)
-Label(master=win,text='deg_wave:', background=bg_frame).place(x=x0+1*sp, y=(sl+5)*lh)
-Label(master=win,text='deg_bkg:', background=bg_frame).place(x=x0+1*sp, y=(sl+6)*lh)
+Label(fr2, text='Options data reduction', font=(font_type, font_size, 'bold'), background=bg_frame).grid(row=0, column=0, sticky="nw", padx=(xy0,0), pady=(xy0,y1), columnspan=3)
+Label(fr3, text='Options plotting data', font=(font_type, font_size, 'bold'), background=bg_frame).grid(row=0, column=0, sticky="nw", padx=(xy0,0), pady=(xy0,y1), columnspan=3)
+Label(fr2, text='Advanced', font=(font_type, font_size, 'bold'), background=bg_frame).grid(row=9, column=0, sticky="nw", padx=(xy0,x1), pady=y1)
+Label(fr4, text='Molecular specifies (for telluric add)', font=(font_type, font_size, 'bold'), background=bg_frame).grid(row=0, column=0, sticky="nw", padx=(xy0,0), pady=(xy0,y1), columnspan=3)
+Label(fr4, text='Optical: H2O O2; NIR: H2O CH4 N2O CO2 CO', background=bg_frame).grid(row=1, column=0, sticky="nw", padx=(xy0,0), pady=y1, columnspan=3)
 
-Label(master=win,text='kapsig:', background=bg_frame).place(x=x0+0*sp, y=(sl+3)*lh)
-Label(master=win,text='oversampl:', background=bg_frame).place(x=x0+0*sp, y=(sl+4)*lh)
-Label(master=win,text='stepRV:', background=bg_frame).place(x=x0+0*sp, y=(sl+5)*lh)
+Label(fr2, text='nset:', background=bg_frame).grid(row=1, column=0, sticky="nw", padx=(xy0,x1), pady=y1)
+Label(fr2, text='oset:', background=bg_frame).grid(row=2, column=0, sticky="nw", padx=(xy0,x1), pady=y1)
+Label(fr2, text='chunks:', background=bg_frame).grid(row=3, column=0, sticky="nw", padx=(xy0,x1), pady=y1)
 
-Label(master=win,text='telluric:', background=bg_frame).place(x=x0+0*sp, y=(sl+6)*lh)
-Label(master=win,text='tsig:', background=bg_frame).place(x=x0+0*sp, y=(sl+7)*lh)
+Label(fr2,text='IP:', background=bg_frame).grid(row=1, column=2, sticky="nw", padx=x1, pady=y1)
+Label(fr2,text='iphs:', background=bg_frame).grid(row=2, column=2, sticky="nw", padx=x1, pady=y1)
+Label(fr2,text='rvguess:', background=bg_frame).grid(row=3, column=2, sticky="nw", padx=x1, pady=y1)
+Label(fr2,text='vcut:', background=bg_frame).grid(row=4, column=2, sticky="nw", padx=x1, pady=y1)
 
-Label(master=win, text='Plot fitted chunks', font=(font_type, font_size, 'bold'), background=bg_frame).place(x=x0+sp+sps, y=(sl+6)*lh)
+Label(fr2,text='deg_norm:', background=bg_frame).grid(row=5, column=2, sticky="nw", padx=x1, pady=y1)
+Label(fr2,text='deg_wave:', background=bg_frame).grid(row=6, column=2, sticky="nw", padx=x1, pady=y1)
+Label(fr2,text='deg_bkg:', background=bg_frame).grid(row=7, column=2, sticky="nw", padx=x1, pady=y1)
+
+Label(fr2,text='kapsig:', background=bg_frame).grid(row=4, column=0, sticky="nw", padx=(xy0,x1), pady=y1)
+Label(fr2,text='oversampl:', background=bg_frame).grid(row=5, column=0, sticky="nw", padx=(xy0,x1), pady=y1)
+Label(fr2,text='stepRV:', background=bg_frame).grid(row=6, column=0, sticky="nw", padx=(xy0,x1), pady=y1)
+
+Label(fr2,text='telluric:', background=bg_frame).grid(row=7, column=0, sticky="nw", padx=(xy0,x1), pady=y1)
+Label(fr2,text='tsig:', background=bg_frame).grid(row=8, column=0, sticky="nw", padx=(xy0,x1), pady=y1)
+
+Label(fr3, text='Plot fitted chunks', font=(font_type, font_size, 'bold'), background=bg_frame).grid(row=7, column=0, sticky="nw", padx=(xy0,x1), pady=y1, columnspan=3)
 
 ###### MAIN ######
 
