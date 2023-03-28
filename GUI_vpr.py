761d21cb41a568100bdc364818633da502661e3f
Jana Köhler <77435060+JanaKoehler1985@users.noreply.github.com>
2023-03-28 11:15:23 +0200

    chore: Few layout changes to make the look more consistence

--- GUI_vpr.py
+++ GUI_vpr.py
@@ -5,7 +5,7 @@
 
 import os
 from tkinter import *
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename, askdirectory
 from tkinter import ttk
 from tkinter.scrolledtext import ScrolledText
 from hbox import Help_Box
@@ -134,6 +134,12 @@ def bt_par(e_file):
         e_file.delete(0, END)
         e_file.insert(0, file)
 
+def bt_res(e_dir):
+    edir = askdirectory()
+    if edir:
+        e_dir.delete(0, END)
+        e_dir.insert(0, edir)
+
 def bt_swap():
     f1 = filename1.get()
     f2 = filename2.get()
@@ -202,9 +208,9 @@ def create_cb(orders_all, cbo, cb_orders, frm):
 
     for i, o in enumerate(orders_all):
         cb_orders.append(IntVar())
-        yi, xi =  divmod(i, 5)
+        yi, xi =  divmod(i, 6)
         c = ttk.Checkbutton(frm, text="  "+str(o),  variable=cb_orders[i])
-        c.grid(row=yi, column=xi, sticky="nw", padx=15, pady=2)
+        c.grid(row=yi+1, column=xi, sticky="nw", padx=8, pady=2)
         cbo.append(c)
         cb_orders[i].set(1)
         cb_orders[i].trace("w", update_changes)
@@ -227,8 +233,8 @@ def get_parameters():
     cb_parx, cb_pary = StringVar(), StringVar()
 
     # parameters x axis
-    l = LabelFrame(frm_par1, text='', bg=bg_frame, bd=2)
-    l.grid(row=1, column=0, sticky="news", padx=5, pady=5, ipady=5, columnspan=1)
+    l = LabelFrame(frm_par, text='test', bg=bg_frame, bd=2)
+    l.grid(row=4, column=0, sticky="news", padx=10, pady=5, ipady=5, columnspan=1)
 
     for i, c in enumerate(colnames[:3]):
         cc = ttk.Radiobutton(l, text="  "+str(c),  variable=cb_parx, value=c, style = 'par.TRadiobutton')
@@ -240,15 +246,16 @@ def get_parameters():
     par_groups = ['rv', 'norm', 'wave', 'ip', 'atm', 'bkg']
     for i, cn in enumerate(par_groups):
         yi, xi =  divmod(i, 6)
-        l = LabelFrame(frm_par1, text=cn, bg=bg_frame, bd=2)
-        l.grid(row=3+yi, column=xi, sticky="news", padx=5, pady=5, ipady=5, columnspan=1)
-        frm_par1.grid_columnconfigure(i, weight=1)
+        l = LabelFrame(frm_par, text=cn, bg=bg_frame, bd=2)        
+        l.grid(row=6+yi, column=xi, sticky="news", padx=10, pady=5, ipady=5, columnspan=1)
+        frm_par.grid_columnconfigure(i, weight=1)
         lfr_par.append(l)
 
     for i, c in enumerate(colnames[4:][::2]):
         pos = par_groups.index(re.sub(r'[0-9]', '', c))
         cc = ttk.Radiobutton(lfr_par[pos], text="  "+str(c),  variable=cb_pary, value=c, style = 'par.TRadiobutton')
         cc.grid(row=i+5, column=0, sticky="new", padx=15, pady=2)
+    cb_pary.set('ip0')
     
     cb_parx.trace("w", lambda *args: call_vpr('-plot par '))
     cb_pary.trace("w", lambda *args: call_vpr('-plot par '))
@@ -301,27 +308,31 @@ font_size = 12
 bg_frame = '#f1f1f1'    # bg color big frame
 bg_color = '#e6e1e1'    # bg color small frames
 
-win_width = 840     # width of GUI window
-win_high = 820      # height of GUI window
+win_width = 880     # width of GUI window
+win_high = 830      # height of GUI window
 
 win = Tk()
 win.title('Gui VPR')
 win.geometry("{}x{}".format(win_width, win_high))
-win.configure(background=ttk.Style().lookup('TFrame', 'background'))   # same bg-color for win and Frame
+win.configure(background=ttk.Style().lookup('TFrame', 'background'), padx=3, pady=6)   # same bg-color for win and Frame
 
 style = ttk.Style(win)
 style.configure('lefttab.TNotebook', tabposition='nw')
-style.configure('TNotebook.Tab', font=(font_type, font_size+2, 'bold'))
+style.configure('TNotebook.Tab', font=(font_type, font_size+1, 'bold'))
+style.configure('TFrame', background=bg_color)
+style.map('TNotebook.Tab', background= [("selected", bg_color)])
+#style.configure('Frame1.TFrame', background='red')
 
 tabControl = ttk.Notebook(win, style='lefttab.TNotebook')
 
-tab_rv = ttk.Frame(tabControl)
-tab_par = ttk.Frame(tabControl)
-tab_res = ttk.Frame(tabControl)
 
-tabControl.add(tab_rv, text ='RV plots')
-tabControl.add(tab_par, text ='Parameters')
-tabControl.add(tab_res, text ='Residuals')
+tab_rv = ttk.Frame(tabControl, style='TFrame')
+tab_par = ttk.Frame(tabControl, style='TFrame')
+tab_res = ttk.Frame(tabControl, style='TFrame')
+
+tabControl.add(tab_rv, text ='  RV plots  ')
+tabControl.add(tab_par, text ='  Parameters  ')
+tabControl.add(tab_res, text ='  Residuals  ')
 tabControl.pack(expand=1, fill="both", side='top')
 
 
@@ -338,43 +349,41 @@ xy0 = 10
 def new(event):
     win_width = win.winfo_width()
     win_height = event.height
-    frm_rv.config(width=win_width-20)
-    frm_par.config(width=win_width-20)
-    frm_res.config(width=win_width-20)
+    frm_rv.config(width=win_width-26)
+    frm_par.config(width=win_width-26)
+    frm_res.config(width=win_width-26)
 
 win.bind("<Configure>", new)
 win.columnconfigure(0, weight=1)
 
 # Frame of RV Tab
-frm_rv = Frame(tab_rv, height=fr_high, width=win_width-20, bg=bg_frame, bd=2, relief='groove')
+frm_rv = Frame(tab_rv, height=fr_high, width=win_width-26, bg=bg_frame, bd=2, relief='groove')
 frm_rv.grid(row=0, column = 0, sticky="news",padx=10,pady=10)
 frm_rv.grid_propagate(False)
 frm_rv.columnconfigure(0, weight=1)
 frm_rv.columnconfigure(1, weight=1)
 
 # Frame reading in rvo files
-frm_rv1 = Frame(frm_rv, bg=bg_frame, bd=2, relief='groove')
-#frm_rv1.pack(padx=10, pady=(10,10), ipady = (3), fill='both', side='top')
-frm_rv1.grid(row=0, column=0, sticky="news", padx=10, pady=10, ipady=5, columnspan=2)
+frm_rv1 = LabelFrame(frm_rv, text="Input", bg=bg_frame, bd=2)
+frm_rv1.grid(row=1, column=0, sticky="news", padx=10, pady=5, ipady=5, columnspan=2)
 frm_rv1.grid_columnconfigure(1, weight=1)
 
 # Sub Frames for RV parameters
 lfr_oset1 = LabelFrame(frm_rv, text="oset rvo 1", bg=bg_frame, bd=2)
-lfr_oset1.grid(row=1, column=0, sticky="news", padx=(10,0), pady=5, ipady=5, columnspan=1)
+lfr_oset1.grid(row=2, column=0, sticky="news", padx=(10,0), pady=5, ipady=5, columnspan=1)
 
 lfr_oset2 = LabelFrame(frm_rv, text="oset rvo 2", bg=bg_frame, bd=2)
-lfr_oset2.grid(row=1, column=1, sticky="news", padx=10, pady=5, ipady=5, columnspan=1)
+lfr_oset2.grid(row=2, column=1, sticky="news", padx=10, pady=5, ipady=5, columnspan=1)
 
 lfr_cen = LabelFrame(frm_rv, text="center RV values", bg=bg_frame, bd=2)
-lfr_cen.grid(row=2, column=0, sticky="news", padx=(10,0), pady=5, ipady=5, columnspan=1)
+lfr_cen.grid(row=3, column=0, sticky="news", padx=(10,0), pady=5, ipady=5, columnspan=1)
 
 lfr_plot = LabelFrame(frm_rv, text="Plotting Options", bg=bg_frame, bd=2)
-lfr_plot.grid(row=2, column=1, sticky="news", padx=10, pady=5, ipady=5, columnspan=1)
+lfr_plot.grid(row=3, column=1, sticky="news", padx=10, pady=5, ipady=5, columnspan=1)
 
 lfr_other = LabelFrame(frm_rv, text="Other", bg=bg_frame, bd=2)
-lfr_other.grid(row=3, column=0, sticky="news", padx=(10,10), pady=5, ipady=5, columnspan=2)
-
-#lfr_other.grid_columnconfigure(1, weight=1)
+lfr_other.grid(row=4, column=0, sticky="news", padx=(10,10), pady=5, ipady=5, columnspan=2)
+lfr_other.grid_columnconfigure(1, weight=1)
 #lfr_other.grid_columnconfigure(2, weight=1)
 
 for cc in range(0,5,1):
@@ -383,17 +392,17 @@ for cc in range(0,5,1):
 
 
 # Frame for Parameter plots
-frm_par = Frame(tab_par, height=100, width=win_width-20, bg=bg_frame, bd=2, relief='groove')
-frm_par.grid(row=0, column = 0, sticky="news",padx=10,pady=(10,0))
-frm_par.grid_propagate(False)
-frm_par.grid_columnconfigure(1, weight=1)
 
-frm_par1 = Frame(tab_par, height=fr_high-100, width=win_width-20, bg=bg_frame, bd=2, relief='groove')
-frm_par1.grid(row=1, column = 0, sticky="news",padx=10,pady=(0,10))
-frm_par1.grid_propagate(False)
+frm_par = Frame(tab_par, height=fr_high, width=win_width-26, bg=bg_frame, bd=2, relief='groove')
+frm_par.grid(row=0, column = 0, sticky="news", padx=10, pady=10)
+frm_par.grid_propagate(False)
+
+lpar = LabelFrame(frm_par, text='Input',  bg=bg_frame, bd=2)
+lpar.grid(row=1, column = 0, sticky="news", padx=10, pady=5, ipady=5, columnspan=10)
+lpar.grid_columnconfigure(1, weight=1)
 
 # Frame for Residual Tab
-frm_res = Frame(tab_res, height=fr_high, width=win_width-20, bg=bg_frame, bd=2, relief='groove')
+frm_res = Frame(tab_res, height=fr_high, width=win_width-26, bg=bg_frame, bd=2, relief='groove')
 frm_res.grid(row=0, column = 0, sticky="news",padx=10,pady=10)
 frm_res.grid_propagate(False)
 frm_res.grid_columnconfigure(0, weight=1)
@@ -420,60 +429,67 @@ b_exit = ttk.Button(master=win, text='EXIT', command = bt_exit)
 b_exit.pack(side='right', anchor="s", padx=20, pady = 20)
 
 b_rvo1 = Button(frm_rv1, text='Search data file', command = bt_rvo1, background="#fdfdfd", width=15)
-b_rvo1.grid(row=0, column=2, sticky="ne", padx=xy0, pady=(xy0,3))
+b_rvo1.grid(row=0, column=2, sticky="ne", padx=xy0, pady=(0,3))
 
 b_rvo2 = Button(frm_rv1,text='Search data file', command = bt_rvo2, background="#fdfdfd", width=15)
 b_rvo2.grid(row=1, column=2, sticky="ne", padx=xy0, pady=(3))
 
-b_swap = Button(frm_rv1, text='swap', command = bt_swap, background="#fdfdfd", width=15)
+b_swap = Button(frm_rv1, text='Swap', command = bt_swap, background="#fdfdfd", width=15)
 b_swap.grid(row=2, column=1, sticky="ne", padx=xy0, pady = 3, rowspan=2)
 
-b_save = ttk.Button(lfr_other, text='Save', command = lambda: call_vpr('-save'))
-b_save.grid(row=1, column=2, sticky="nw", padx=xy0)
+b_save = Button(lfr_other, text='Save', command = lambda: call_vpr('-save'), background="#fdfdfd", width=15)
+b_save.grid(row=1, column=2, sticky="ne", padx=xy0)
 
 b_cmp = ttk.Button(frm_rv1, text='Compare', command = lambda: call_vpr(cmp=True), width=15)
 b_cmp.grid(row=2, column=2, sticky="ne", padx=xy0, pady = 3)
 
 conf = {'side': 'right', 'anchor': 's', 'padx': (0,20), 'pady': 20}
 
-b_nrvo = ttk.Button(frm_rv, text='Plot no-rv', command = lambda: call_vpr('-plot nrvo'))
+b_nrvo = ttk.Button(frm_rv, text='Plot no-rv', width = 15, command = lambda: call_vpr('-plot nrvo'))
 b_nrvo.pack(**conf)
 
-b_rvo = ttk.Button(frm_rv, text='Plot o-rv', command = lambda: call_vpr('-plot rvo'))
+b_rvo = ttk.Button(frm_rv, text='Plot o-rv', width = 15, command = lambda: call_vpr('-plot rvo'))
 b_rvo.pack(**conf)
 
-b_rvbjd = ttk.Button(frm_rv, text='Plot BJD-RV', command = call_vpr)
+b_rvbjd = ttk.Button(frm_rv, text='Plot BJD-RV', width = 15, command = call_vpr)
 b_rvbjd.pack(**conf)
 
-b_fpar = Button(frm_par,text='Search data file', command = lambda: bt_par(e_parf), background="#fdfdfd", width=15)
+b_fpar = Button(lpar,text='Search data file', command = lambda: bt_par(e_parf), background="#fdfdfd", width=15)
 b_fpar.grid(row=1, column=2, sticky="ne", padx=10, pady=0)
+
+b_par = ttk.Button(frm_par, text='Plot par', width = 15, command = lambda: call_vpr('-plot par '))
+#b_res.grid(row=12, column=1, sticky="se", padx=xy0, pady=(xy0,xy0))
+b_par.pack(**conf)
 
 #b_cmp.pack(**{**conf, 'side': 'right', 'padx': 20})
 
 b_oset1_a = ttk.Button(lfr_oset1, text='select all', style = 'B2.TButton', command = lambda: set_oset(cb_orders1, 1))
-b_oset1_a.grid(row=8, column=0, sticky="sw", padx=xy0, pady=5, columnspan=2)
+b_oset1_a.grid(row=0, column=0, sticky="ne", padx=10, pady=(0,5), columnspan=6)
 b_oset1_n = ttk.Button(lfr_oset1, text='select none', style = 'B2.TButton', command = lambda: set_oset(cb_orders1, 0))
-b_oset1_n.grid(row=8, column=2, sticky="sw", padx=xy0, pady=5, columnspan=2)
+b_oset1_n.grid(row=0, column=0, sticky="ne", padx=100, pady=(0,5), columnspan=6)
 
 b_oset2_a = ttk.Button(lfr_oset2, text='select all', style = 'B2.TButton', command = lambda: set_oset(cb_orders2, 1))
-b_oset2_a.grid(row=8, column=0, sticky="sw", padx=xy0, pady=5, columnspan=2)
+b_oset2_a.grid(row=0, column=0, sticky="ne", padx=10, pady=(0,5), columnspan=6)
 b_oset2_n = ttk.Button(lfr_oset2, text='select none', style = 'B2.TButton', command = lambda: set_oset(cb_orders2, 0))
-b_oset2_n.grid(row=8, column=2, sticky="sw", padx=xy0, pady=5, columnspan=2)
+b_oset2_n.grid(row=0, column=0, sticky="ne", padx=100, pady=(0,5), columnspan=6)
 
-b_res = ttk.Button(frm_res, text='Plot res', command = lambda: call_vpr(res=True))
+
+b_fres = Button(lres,text='Search directory', command = lambda: bt_res(e_dir), background="#fdfdfd", width=15)
+b_fres.grid(row=1, column=2, sticky="ne", padx=10, pady=0)
+
+b_res = ttk.Button(frm_res, text='Plot res', width = 15, command = lambda: call_vpr(res=True))
 #b_res.grid(row=12, column=1, sticky="se", padx=xy0, pady=(xy0,xy0))
 b_res.pack(**conf)
 
 b_reso_a = ttk.Button(lreso, text='select all', style = 'B2.TButton', command = lambda: set_oset(cb_reso, 1))
-b_reso_a.grid(row=0, column=0, sticky="ne", padx=20, pady=5, columnspan=6)
+b_reso_a.grid(row=0, column=0, sticky="ne", padx=10, pady=(0,5), columnspan=6)
 b_reso_n = ttk.Button(lreso, text='select none', style = 'B2.TButton', command = lambda: set_oset(cb_reso, 0))
-b_reso_n.grid(row=0, column=0, sticky="ne", padx=110, pady=5, columnspan=6)
+b_reso_n.grid(row=0, column=0, sticky="ne", padx=100, pady=(0,5), columnspan=6)
 
 b_resn_a = ttk.Button(lresn, text='select all', style = 'B2.TButton', command = lambda: set_oset(cb_resn, 1))
-b_resn_a.grid(row=0, column=0, sticky="ne", padx=20, pady=5, columnspan=6)
+b_resn_a.grid(row=0, column=0, sticky="ne", padx=10, pady=(0,5), columnspan=6)
 b_resn_n = ttk.Button(lresn, text='select none', style = 'B2.TButton', command = lambda: set_oset(cb_resn, 0))
-b_resn_n.grid(row=0, column=0, sticky="ne", padx=110, pady=5, columnspan=6)
-
+b_resn_n.grid(row=0, column=0, sticky="ne", padx=100, pady=(0,5), columnspan=6)
 
 ###### ENTRIES ######
 
@@ -482,8 +498,8 @@ filename2 = StringVar()
 parfile = StringVar()
 
 Label(master=win, text='Current command:').pack(side='top', anchor="w", padx=xy0)
-e_run = ScrolledText(win, background='#f0f0f0')
-e_run.pack(side='left', padx=xy0, pady=(0, xy0))
+e_run = ScrolledText(win, background='#f0f0f0', width=200)
+e_run.pack(side='left', padx=(xy0,60), pady=(0, xy0))
 
 e_rvo1 = Entry(frm_rv1, textvariable=filename1, width=100)
 e_rvo1.insert(0, 'tmp.rvo.dat')
@@ -504,12 +520,12 @@ e_offset.insert(0, 400)
 e_offset.bind("<Return>", (lambda event: update_changes()))
 e_offset.grid(row=1, column=1, sticky="nw", padx=(xy0), pady=(5,0))
 
-e_out = Entry(lfr_other, width=35)
+e_out = Entry(lfr_other, width=350)
 e_out.insert(0, 'tmp.dat')
 e_out.grid(row=1, column=1, sticky="nw", padx=xy0, pady=(5,0), columnspan=1)
 
 
-e_parf = Entry(frm_par, textvariable=parfile, width=120)
+e_parf = Entry(lpar, textvariable=parfile, width=120)
 e_parf.insert(0, 'tmp.par.dat')
 e_parf.bind("<Return>", (lambda event: get_parameters()))
 e_parf.grid(row=1, column=1, sticky="new", padx=xy0, pady=(5))
@@ -524,14 +540,6 @@ e_ressep = Entry(lres, width = 20)
 e_ressep.insert(0, 5)
 e_ressep.bind("<Return>", (lambda event:  call_vpr(res=True)))
 e_ressep.grid(row=2, column=1, sticky="nw", padx=xy0, pady=3)
-#, lambda *args: call_vpr(res=True)
-#e_nset_r = Entry(frm_res)
-#e_nset_r.insert(0, '')
-#e_nset_r.grid(row=2, column=1, sticky="nw", padx=xy0, pady=xy0)
-
-#e_oset_r = Entry(frm_res)
-#e_oset_r.insert(0, '')
-#e_oset_r.grid(row=3, column=1, sticky="nw", padx=xy0, pady=xy0)
 
 
 ###### COMBOBOXES ######
@@ -568,9 +576,11 @@ cb_cmpocen.trace("w", update_changes)
 
 ###### LABELS ######
 
-Label(frm_rv1, text='rvo file 1', font=(font_type, font_size, 'bold'), background=bg_frame).grid(row=0, column=0, sticky="sw", padx=xy0, pady=5)
+Label(frm_rv, text='Plot RVs', font=(font_type, font_size, 'bold'), background=bg_frame).grid(row=0, column=0, sticky="nw", padx=xy0, pady=(xy0,5), columnspan = 2)
 
-Label(frm_rv1, text='rvo file 2', font=(font_type, font_size, 'bold'), background=bg_frame).grid(row=1, column=0, sticky="nw", padx=xy0, pady=5)
+Label(frm_rv1, text='rvo file 1:', font=(font_type, font_size-1, 'bold'), background=bg_frame).grid(row=0, column=0, sticky="sw", padx=xy0, pady=5)
+
+Label(frm_rv1, text='rvo file 2:', font=(font_type, font_size-1, 'bold'), background=bg_frame).grid(row=1, column=0, sticky="nw", padx=xy0, pady=5)
 
 l_sort = Label(lfr_plot, text='sort by:', background=bg_frame)
 l_sort.grid(row=0, column=0, sticky="nw", padx=xy0, pady=(5,0))
@@ -590,23 +600,21 @@ Help_Box(widget = l_out, text = text_from_file("'-save'"))
 
 Label(frm_par, text='Plot parameters', font=(font_type, font_size, 'bold'), background=bg_frame).grid(row=0, column=0, sticky="nw", padx=xy0, pady=xy0, columnspan = 2)
 
-Label(frm_par, text='parfile:', background=bg_frame).grid(row=1, column=0, sticky="nw", padx=xy0, pady=5)
+Label(lpar, text='parfile:', background=bg_frame, font=(font_type, font_size-1, 'bold')).grid(row=1, column=0, sticky="nw", padx=xy0, pady=5)
 
-Label(frm_par1, text='Parameters x-axis', font=(font_type, font_size, 'bold'), background=bg_frame).grid(row=0, column=0, sticky="nw", padx=xy0, pady=xy0, columnspan = 5)
+Label(frm_par, text='Parameters x-axis', font=(font_type, font_size-1, 'bold'), background=bg_frame).grid(row=3, column=0, sticky="nw", padx=xy0, pady=xy0, columnspan = 5)
 
-Label(frm_par1, text='Parameters y-axis', font=(font_type, font_size, 'bold'), background=bg_frame).grid(row=2, column=0, sticky="nw", padx=xy0, pady=xy0, columnspan = 5)
+Label(frm_par, text='Parameters y-axis', font=(font_type, font_size-1, 'bold'), background=bg_frame).grid(row=5, column=0, sticky="nw", padx=xy0, pady=xy0, columnspan = 5)
 
 Label(frm_res, text='Plot residual', font=(font_type, font_size, 'bold'), background=bg_frame).grid(row=0, column=0, sticky="nw", padx=xy0, pady=xy0)
 
 Label(frm_res, text='For large number of nset, it is recommended to first chose one single order before using "select all" on nset.', background=bg_frame, fg='red4').grid(row=2, column=0, sticky="nw", padx=xy0, pady=xy0, columnspan=2)
 
-Label(lres, text='directory:', background=bg_frame).grid(row=1, column=0, sticky="nw", padx=xy0, pady=3)
+Label(lres, text='directory:', background=bg_frame, font=(font_type, font_size-1, 'bold')).grid(row=1, column=0, sticky="nw", padx=xy0, pady=3)
 
 l_ressep = Label(lres, text='offset res:', background=bg_frame)
 l_ressep.grid(row=2, column=0, sticky="nw", padx=xy0, pady=3)
 Help_Box(widget = l_ressep, text = text_from_file("'-ressep'"))
-#Label(frm_res, text='nset:', background=bg_frame).grid(row=2, column=0, sticky="nw", padx=xy0, pady=xy0)
-#Label(frm_res, text='oset:', background=bg_frame).grid(row=3, column=0, sticky="nw", padx=xy0, pady=xy0)
 
 
 ###### MAIN ######
