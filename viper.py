aa4a75bfc7e0cd6540f3f032d23b4b1706e70c92
Jana Köhler <77435060+JanaKoehler1985@users.noreply.github.com>
2023-08-28 09:11:40 +0200

    chore: changing to one CRIRES+ inst file

--- viper.py
+++ viper.py
@@ -47,7 +47,7 @@ c = 299792.458   # [km/s] speed of light
 
 targ = None
 modset = {}   # model setting parameters
-insts = ['TLS', 'CES', 'KECK', 'UVES', 'OES', 'CRIRES', 'cplCRIRES', 'McDonald']
+insts = ['TLS', 'CES', 'KECK', 'UVES', 'OES', 'CRIRES', 'McDonald']
 
 class nameddict(dict):
    """
